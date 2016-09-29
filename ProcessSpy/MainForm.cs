5f38535ed95f393c56e35a2837dde0c6d7c10d80
amenprog <amenprog@gmail.com>
2016-09-29 13:37:14 +0300

    Add project files.

---
+++ ProcessSpy/MainForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessSpy
+{
+    public partial class MainForm : Form
+    {
+
+        private Timer _timer;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _timer = new Timer() { Interval =  1000};
+            _timer.Tick += Tm_Tick;
+            _timer.Start();
+        }
+
+        private void Tm_Tick(object sender, EventArgs e)
+        {
+            NativeMethods.POINT p;
+
+            if (NativeMethods.GetCursorPos(out p))
+            {
+
+                IntPtr hWnd = NativeMethods.WindowFromPoint(p);
+
+                IntPtr processId;
+                NativeMethods.GetWindowThreadProcessId(hWnd, out processId);
+                richTextBox1.Text = Process.GetProcessById(processId.ToInt32()).MainModule.FileName;
+
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _timer.Stop();
+            _timer.Dispose();
+        }
+    }
+}
---
+++ ProcessSpy/NativeMethods.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ProcessSpy
+{
+    static class NativeMethods
+    {
+
+        [DllImport("user32.dll")]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out IntPtr ProcessId);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr WindowFromPoint(POINT Point);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern uint GetWindowModuleFileName(IntPtr hwnd,
+            StringBuilder lpszFileName, uint cchFileNameMax);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out NativeMethods.POINT lpPoint);
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public POINT(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+
+            public static implicit operator System.Drawing.Point(POINT p)
+            {
+                return new System.Drawing.Point(p.X, p.Y);
+            }
+
+            public static implicit operator POINT(System.Drawing.Point p)
+            {
+                return new POINT(p.X, p.Y);
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+    }
+}
